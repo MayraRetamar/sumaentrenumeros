70ebb1a3849044ca9bd2470a9101253c52f2d91b
MayraRetamar <104107235+MayraRetamar@users.noreply.github.com>
2022-05-05 19:54:30 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let dato1: number;
+let dato2: number;
+
+dato1: Number(prompt("ingrese un numero"));
+dato2: Number(prompt("Ingrese el segundo numero"));
+let contador: number;
+let suma: number;
+suma = 0;
+for (let contador: number = dato1; contador <= dato2; contador++) {
+  suma + contador;
+}
+console.log("La suma es " + suma);
