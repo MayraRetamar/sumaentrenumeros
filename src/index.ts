022b2760e1f698c74094045053fe7ebb14002ab7
MayraRetamar <104107235+MayraRetamar@users.noreply.github.com>
2022-05-07 15:25:12 -0300

    Arregloo

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 let dato1: number;
 let dato2: number;
-
-dato1: Number(prompt("ingrese un numero"));
-dato2: Number(prompt("Ingrese el segundo numero"));
 let contador: number;
-let suma: number;
-suma = 0;
+let suma: number = 0;
+dato1 = Number(prompt("Ingrese el primer numero" + contador + ":"));
+dato2 = Number(prompt("Ingrese el segundo numero"));
 for (let contador: number = dato1; contador <= dato2; contador++) {
-  suma + contador;
+  suma += contador;
 }
-console.log("La suma es " + suma);
+console.log("La suma entre numeros es " + suma);
